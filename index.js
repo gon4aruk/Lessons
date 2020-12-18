55126b86e730f2653558ca65071ca559ccb16128
gon4aruk <sashagonch27@gmail.com>
2020-12-18 22:32:45 +0200

    add jest

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const getTotalRevenue = arr => {
-   let result = 0;
-   arr.map(user => result += user.amount);
-   return result;
-}
+const transactionsList = ["  1.9 ", "16.4", 17, " 1 dollar ", "fdd", '23', 4];
 
+const cleanTransactionsList = (transactions) => {
+  const sortList = transactions
+    .map((el) => Number(el))
+    .filter((el) => !Number.isNaN(el))
+    .map((el) => `$${el.toFixed(2)}`);
+  return sortList;
+};
+
+const result = cleanTransactionsList(transactionsList);
+console.log(result);
