f10d8428324278bcb09f15f8ded5564cd4b7e801
gon4aruk <sashagonch27@gmail.com>
2020-12-20 15:30:57 +0200

    lessons unit tests

--- reverseArray.test.js
+++ reverseArray.test.js
@@ -1,7 +1,5 @@
 import { reverseArray } from "/GromCode/JS/lessons/reverseArray.js";
 
-const someArray = [2, 4, 5, 7];
-
 it('slould get "null" if passed not an array', () => {
   const result = reverseArray(2, 4, 5);
 
@@ -9,15 +7,15 @@ it('slould get "null" if passed not an array', () => {
 });
 
 it("slould get reversed array", () => {
-  const result = reverseArray(someArray);
+  const result = reverseArray([2, 4, 5, 7]);
 
   expect(result).toEqual([7, 5, 4, 2]);
 });
 
-it("primary arrray shouldn't change", () => {
+it("primary array shouldn't be changed", () => {
+  const someArray = [2, 4, 5, 7];
   const result = reverseArray(someArray);
-  const primaryArray = [...someArray];
 
   expect(result).toEqual([7, 5, 4, 2]);
-  expect(primaryArray).toEqual([2, 4, 5, 7]);
+  expect(someArray).toEqual([2, 4, 5, 7]);
 });
