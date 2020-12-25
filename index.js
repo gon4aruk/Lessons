549b04643a2b2eaa07c72f3fd8574830b4138637
gon4aruk <sashagonch27@gmail.com>
2020-12-25 17:46:46 +0200

    bind

--- index.js
+++ index.js
@@ -1,93 +1,47 @@
-/* ===> 1 <=== */
-const student = {
-  name: "Tom",
-};
-
-function sayName() {
-  console.log(this.name);
+export function calculator(a, b) {
+  switch (this.operation) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '/':
+      return a / b;
+    case '*':
+      return a * b;
+    default:
+      return NaN;
+  }
 }
 
 /*
- * создайте ф-цию sayStudentName которая будет выводить в консоль имя студента 'Tom'
- * используйте .bind и ф-цию sayName
+ * Ф-ция multiplier должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 2 числа и возвращать из произведение
  */
 
-export const sayStudentName = sayName.bind(student);
-
-/*
- * создайте ф-цию sayBruceName которая будет выводить в консоль имя 'Bruce'
- * используйте ф-цию sayName и .bind с нужным объектом
- */
-
-export const sayBruceName = sayName.bind({ name: "Bruce" });
-
-
-/* ===> 2 <=== */
-const company = {
-  companyName: "Microsoft",
-};
-
-function greeting(firstName, lastName) {
-  console.log(
-    `Hello, ${firstName} ${lastName}. Welcome to the ${this.companyName}`
-  );
-}
-
-/*
- * создайте ф-цию specialGreeting которая будет выводить в консоль
- * 'Hello, Bob Marley. Welcome to the Microsoft`
- * используйте ф-цию greeting и .bind с нужным объектом и аргументами
- * specialGreeting не должна принимать ни одного аргумента
- */
-
-export const specialGreeting = greeting.bind(company, "Bob", "Marley");
-
-/* ===> 3 <=== */
-const country = {
-  countryName: "Ukraine",
-  capital: "Kyiv",
-};
-
-function getPopulation(population) {
-  return `Population in ${this.countryName} is ${population}`;
-}
-
-/*
- * создайте ф-цию getUkrainePopulation которая будет возвращать строку
- * 'Population in Ukraine is 43000`
- * 43000 передавайте в виде числа
- * используйте ф-цию getPopulation и .bind с нужным объектом и аргументами
- * getUkrainePopulation не должна принимать ни одного аргумента
- */
-
-export const getUkrainePopulation = getPopulation.bind(country, 43000);
-
-/* ===> 4 <=== */
-const transaction = {
-  amount: 1200,
-  operation: "sell",
-  currency: "USD",
-  exchange: "NYSE",
-  printTransaction() {
-    console.log(
-      `${this.amount} ${this.currency} - ${this.operation} on ${this.exchange}`
-    );
-  },
-};
-
-const anotherTransaction = {
-  amount: 400,
-  operation: "buy",
-  currency: "USD",
-  exchange: "NASDAQ",
+export const multiplier = (a, b) => {
+  const obj = { operation: "*" }; 
+  return calculator.bind(obj, a, b)();
 };
 
 /*
- * создайте ф-цию printSpecialTransaction которая будет выводить в консоль
- * '400 USD - buy on NASDAQ`
- * используйте метод transaction.printTransaction и .bind с нужным объектом
- * printSpecialTransaction не должна принимать ни одного аргумента
+ * Ф-ция summator должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 2 числа и возвращать из сумму
  */
-export const printSpecialTransaction = transaction.printTransaction.bind(
-  anotherTransaction
-);
+
+export const summator = (a, b) => {
+  const obj = { operation: "+" }; 
+  return calculator.bind(obj, a, b)();
+};
+
+/*
+ * Ф-ция twice должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 1 число и возвращать это число умноженное на 2
+ */
+
+export const twice = (a) => {
+  const obj = { operation: "*" }; 
+  return calculator.bind(obj, a, 2)();
+};
