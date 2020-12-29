ced1bbb8cc8bdbfdab294ce4d5629d62600c17d0
gon4aruk <sashagonch27@gmail.com>
2020-12-29 19:25:53 +0200

    lessons

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-const tasks = [
-  { text: "Buy milk", done: false },
-  { text: "Pick up Tom from airport", done: false },
-  { text: "Visit party", done: false },
-  { text: "Visit doctor", done: true },
-  { text: "Buy meat", done: true },
-];
+export function getTitleElement() {
+  const titleElem = document.querySelector(".title");
+  console.dir(titleElem);
+  return titleElem;
+}
 
-const renderListItem = (itemList) => {
-  const itemListElem = document.querySelector(".list");
-
-  const itemsListElements = itemList.map(({ text, done }) => {
-    const listItem = document.createElement("li");
-    listItem.classList.add("list__item");
-    const checkbox = document.createElement("input");
-    checkbox.classList.add("list__item-checkbox");
-    checkbox.setAttribute("type", "checkbox");
-    if (done) {
-      listItem.classList.add("list__item_done");
-    }
-    checkbox.checked = done;
-    listItem.append(checkbox, text)
-    return listItem;
-  });
-
-  return itemListElem.append(...itemsListElements);
-};
-
-renderListItem(tasks);
+export function getInputElement() {
+  const inputElem = document.querySelector("input");
+  console.dir(inputElem);
+  return inputElem;
+}
