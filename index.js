5fe62be24af9fd2cad7799a52af87ca8da5a0b4c
gon4aruk <sashagonch27@gmail.com>
2020-12-29 19:46:16 +0200

    lessons

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-export function getTitle() {
-  const titleElem = document.querySelector(".title");
-  return titleElem.textContent;
-}
-
-export function getDescription() {
-  const aboutElem = document.querySelector(".about");
-  return aboutElem.innerText;
-}
-
-export function getPlans() {
-  const plansElem = document.querySelector(".plans");
-  return plansElem.innerHTML;
-}
-
-export function getGoal() {
-  const goalElem = document.querySelector(".goal");
-  return goalElem.outerHTML;
+export const setTitle = (text) => {
+  const titleElem = document.querySelector('.title');
+  titleElem.textContent = text;
 }
