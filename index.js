fa00fa30dfb59948e87d7490bc5a0c1974dd00ac
gon4aruk <sashagonch27@gmail.com>
2020-12-30 22:14:33 +0200

    lessons

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-export const createButton = (text) => {
-  const body = document.querySelector('body');
-  const button = document.createElement('button');
-  button.textContent = text;
-  body.append(button);
-};
+const finishList = () => {
+  const itemsList = document.querySelector(".list");
+  const itemsListArray = Array.from(document.querySelectorAll("li"));
+  const itemsText = itemsListArray.map((elem) => elem.textContent);
+  if (itemsListArray.length < 8) {
+    for (let i = 1; i <= 8; i += 1) {
+      if (!itemsText.includes(String(i))) {
+        const listItem = document.createElement("li");
+        listItem.textContent = i;
+        itemsList.append(listItem);
+      }
+    }
+  }
 
+  const itemsListArrayNew = Array.from(document.querySelectorAll("li"));
+  const itemsListArraySorted = itemsListArrayNew.sort(
+    (elem1, elem2) => elem1.textContent - elem2.textContent
+  );
+  itemsList.innerHTML = "";
+  itemsList.append(...itemsListArraySorted);
+};
