1ca41593eb5754da185194738476933b0528f1a9
gon4aruk <sashagonch27@gmail.com>
2020-12-31 11:28:54 +0200

    lessons

--- index.js
+++ index.js
@@ -2,5 +2,3 @@ export const getSection = (num) => {
   const span = document.querySelector(`span[data-number="${num}"]`);
   return span.closest(`.box`).dataset.section;
 };
-
-console.log(getSection(5));
