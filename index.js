9c7fe89d8b365a208efe7b82193f63318a352781
gon4aruk <sashagonch27@gmail.com>
2020-12-31 10:35:21 +0200

    lessons

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const squaredNumbers = () => {
+export const squaredNumbers = () => {
   const numbers = document.querySelectorAll(".number");
   for (let i = 0; i < numbers.length; i += 1) {
     const numberDataValue = numbers[i].dataset.number;
