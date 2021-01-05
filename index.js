be87b4b82421f2fd3d4e6cf2ec0f0713d34a5c4b
gon4aruk <sashagonch27@gmail.com>
2021-01-05 21:37:17 +0200

    lessons

--- index.js
+++ index.js
@@ -1,84 +1,20 @@
-const listElem = document.querySelector(".list");
+const getDiff = (startDate, endDate) => {
+  let diff = (endDate.getTime() - startDate.getTime()) / 1000;
 
-const tasks = [
-  { text: "Buy milk", done: false, id: Math.round(Math.random() * 100) },
-  {
-    text: "Pick up Tom from airport",
-    done: false,
-    id: Math.round(Math.random() * 100),
-  },
-  { text: "Visit party", done: false, id: Math.round(Math.random() * 100) },
-  { text: "Visit doctor", done: true, id: Math.round(Math.random() * 100) },
-  { text: "Buy meat", done: true, id: Math.round(Math.random() * 100) },
-];
+  const days = Math.floor(diff / (60 * 60 * 24));
+  diff -= days * 60 * 60 * 24;
 
-const renderTasks = (tasksList) => {
-  const tasksElems = tasksList
-    .sort((a, b) => a.done - b.done)
-    .map(({ text, done, id }) => {
-      const listItemElem = document.createElement("li");
-      listItemElem.classList.add("list__item");
-      const checkbox = document.createElement("input");
-      checkbox.setAttribute("type", "checkbox");
-      checkbox.checked = done;
-      checkbox.dataset.id = id;
-      checkbox.classList.add("list__item-checkbox");
-      if (done) {
-        listItemElem.classList.add("list__item_done");
-      }
-      listItemElem.append(checkbox, text);
+  const hours = Math.floor(diff / 3600);
+  diff -= hours * 3600;
 
-      return listItemElem;
-    });
+  const minutes = Math.floor(diff / 60);
+  diff -= minutes * 60;
 
-  listElem.append(...tasksElems);
+  const seconds = diff;
+
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 };
 
-renderTasks(tasks);
-
-const onCheckboxClick = (event) => {
-  const isCheckbox = event.target.classList.contains("list__item-checkbox");
-
-  if (!isCheckbox) {
-    return;
-  }
-
-  const listItemSelected = event.target.closest(".list__item");
-  const checkboxId = event.target.dataset.id;
-
-  if (event.target.value) {
-    listItemSelected.classList.toggle("list__item_done");
-    const taskElem = tasks.find(({ id }) => id === Number(checkboxId));
-    if (taskElem.done) {
-      taskElem.done = false;
-    } else {
-      taskElem.done = true;
-    }
-  }
-
-  listElem.textContent = "";
-  renderTasks(tasks);
-};
-
-listElem.addEventListener("click", onCheckboxClick);
-
-const inputElem = document.querySelector(".task-input");
-const buttonElem = document.querySelector(".btn");
-
-const onButtonClick = () => {
-  if (inputElem.value === "") {
-    return;
-  }
-  const newTask = {
-    text: inputElem.value,
-    done: false,
-    id: Math.round(Math.random() * 100),
-  };
-
-  inputElem.value = "";
-  tasks.unshift(newTask);
-  listElem.textContent = "";
-  renderTasks(tasks);
-};
-
-buttonElem.addEventListener("click", onButtonClick);
+console.log(
+  getDiff(new Date(2019, 0, 1, 20, 30), new Date(2020, 0, 1, 20, 30))
+);
