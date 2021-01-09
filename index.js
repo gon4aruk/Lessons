c4b438a7b8d9f5073ab49362a5f9a370fec315be
gon4aruk <sashagonch27@gmail.com>
2021-01-09 16:11:46 +0200

    todo_list

--- index.js
+++ index.js
@@ -1,21 +1,115 @@
-export const getDiff = (startDate, endDate) => {
-  let difference = Math.abs(endDate.getTime() - startDate.getTime());
+const listElem = document.querySelector(".list");
 
-  const MILISECONDS_IN_DAY = 1000 * 60 * 60 * 24;
-  const MILISECONDS_IN_HOUR = 1000 * 60 * 60;
-  const MILISECONDS_IN_MINUTE = 1000 * 60;
-  const MILISECONDS_IN_SECOND = 1000;
+const tasks = [
+  {
+    text: "Buy milk",
+    done: false,
+    id: Math.round(Math.random() * 1000),
+    timeOfChange: new Date(),
+  },
+  {
+    text: "Pick up Tom from airport",
+    done: false,
+    id: Math.round(Math.random() * 1000),
+    timeOfChange: new Date(),
+  },
+  {
+    text: "Visit party",
+    done: false,
+    id: Math.round(Math.random() * 1000),
+    timeOfChange: new Date(),
+  },
+  {
+    text: "Visit doctor",
+    done: true,
+    id: Math.round(Math.random() * 1000),
+    timeOfChange: new Date(),
+  },
+  {
+    text: "Buy meat",
+    done: true,
+    id: Math.round(Math.random() * 1000),
+    timeOfChange: new Date(),
+  },
+];
 
-  const days = Math.floor(difference / MILISECONDS_IN_DAY);
-  difference = Math.floor(difference % MILISECONDS_IN_DAY);
+// render tasks
 
-  const hours = Math.floor(difference / MILISECONDS_IN_HOUR);
-  difference = Math.floor(difference % MILISECONDS_IN_HOUR);
+const renderTasks = (tasksList) => {
+  const tasksElems = tasksList
+    .sort((el1, el2) => el2.timeOfChange - el1.timeOfChange)
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done, id }) => {
+      const listItemElem = document.createElement("li");
+      listItemElem.classList.add("list__item");
+      const checkbox = document.createElement("input");
+      checkbox.setAttribute("type", "checkbox");
+      checkbox.checked = done;
+      checkbox.dataset.id = id;
+      checkbox.classList.add("list__item-checkbox");
+      if (done) {
+        listItemElem.classList.add("list__item_done");
+      }
+      listItemElem.append(checkbox, text);
 
-  const minutes = Math.floor(difference / MILISECONDS_IN_MINUTE);
-  difference = Math.floor(difference % MILISECONDS_IN_MINUTE);
+      return listItemElem;
+    });
 
-  const seconds = difference / MILISECONDS_IN_SECOND;
-
-  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  listElem.append(...tasksElems);
 };
+
+renderTasks(tasks);
+
+// on checkbox click
+
+const onCheckboxClick = (event) => {
+  const isCheckbox = event.target.classList.contains("list__item-checkbox");
+
+  if (!isCheckbox) {
+    return;
+  }
+
+  const selectedListItem = event.target.closest(".list__item");
+  selectedListItem.classList.toggle("list__item_done");
+
+  const checkboxId = event.target.dataset.id;
+  const taskElemInArray = tasks.find(({ id }) => id === Number(checkboxId));
+  taskElemInArray.timeOfChange = new Date();
+
+  if (taskElemInArray.done) {
+    taskElemInArray.done = false;
+  } else {
+    taskElemInArray.done = true;
+  }
+
+  listElem.textContent = "";
+  renderTasks(tasks);
+};
+
+listElem.addEventListener("click", onCheckboxClick);
+
+// add new task
+
+const inputElem = document.querySelector(".task-input");
+const buttonElem = document.querySelector(".btn");
+
+const onButtonClick = () => {
+  if (inputElem.value === "") {
+    return;
+  }
+
+  const newTask = {
+    text: inputElem.value,
+    done: false,
+    id: Math.round(Math.random() * 1000),
+    timeOfChange: new Date(),
+  };
+  tasks.unshift(newTask);
+
+  inputElem.value = "";
+
+  listElem.textContent = "";
+  renderTasks(tasks);
+};
+
+buttonElem.addEventListener("click", onButtonClick);
