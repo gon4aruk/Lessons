25d501417b5aa6cee228faa510f967b7ec84fe2d
gon4aruk <sashagonch27@gmail.com>
2021-01-31 10:29:39 +0200

    lesson

--- index.js
+++ index.js
@@ -1,121 +1,16 @@
-const listElem = document.querySelector(".list");
-
-const tasks = [
-  {
-    text: "Buy milk",
-    done: false,
-    id: Math.round(Math.random() * 1000),
-    timeOfChange: new Date(),
-  },
-  {
-    text: "Pick up Tom from airport",
-    done: false,
-    id: Math.round(Math.random() * 1000),
-    timeOfChange: new Date(),
-  },
-  {
-    text: "Visit party",
-    done: false,
-    id: Math.round(Math.random() * 1000),
-    timeOfChange: new Date(),
-  },
-  {
-    text: "Visit doctor",
-    done: true,
-    id: Math.round(Math.random() * 1000),
-    timeOfChange: new Date(),
-  },
-  {
-    text: "Buy meat",
-    done: true,
-    id: Math.round(Math.random() * 1000),
-    timeOfChange: new Date(),
-  },
-];
-
-// render tasks
-
-const createCheckbox = (done, id) => {
-  const checkboxElem = document.createElement("input");
-  checkboxElem.setAttribute("type", "checkbox");
-  checkboxElem.classList.add("list__item-checkbox");
-  checkboxElem.checked = done;
-  checkboxElem.dataset.id = id;
-  return checkboxElem;
+export const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (error) {
+      newValue = value;
+    }
+    return {
+      ...acc,
+      [key]: newValue,
+    };
+  }, {});
 };
 
-const renderTasks = (tasksList) => {
-  const tasksElems = tasksList
-    .sort(
-      (el1, el2) => el1.done - el2.done || el2.timeOfChange - el1.timeOfChange
-    )
-    .map(({ text, done, id }) => {
-      const listItemElem = document.createElement("li");
-      listItemElem.classList.add("list__item");
-      if (done) {
-        listItemElem.classList.add("list__item_done");
-      }
-
-      const checkbox = createCheckbox(done, id);
-      listItemElem.append(checkbox, text);
-
-      return listItemElem;
-    });
-
-  listElem.append(...tasksElems);
-};
-
-renderTasks(tasks);
-
-// on checkbox click
-
-const onCheckboxClick = (event) => {
-  const isCheckbox = event.target.classList.contains("list__item-checkbox");
-
-  if (!isCheckbox) {
-    return;
-  }
-
-  const checkboxId = event.target.dataset.id;
-  const taskElemInArray = tasks.find(({ id }) => id === Number(checkboxId));
-  taskElemInArray.timeOfChange = new Date();
-
-  if (taskElemInArray.done) {
-    taskElemInArray.done = false;
-  } else {
-    taskElemInArray.done = true;
-  }
-
-  event.target.closest(".list__item").classList.toggle("list__item_done");
-
-  listElem.innerHTML = "";
-  renderTasks(tasks);
-};
-
-listElem.addEventListener("click", onCheckboxClick);
-
-// add new task
-
-const inputElem = document.querySelector(".task-input");
-const buttonElem = document.querySelector(".btn");
-
-const onButtonClick = () => {
-  if (inputElem.value === "") {
-    return;
-  }
-
-  const newTask = {
-    text: inputElem.value,
-    done: false,
-    id: Math.round(Math.random() * 1000),
-    timeOfChange: new Date(),
-  };
-  tasks.unshift(newTask);
-
-  inputElem.value = "";
-
-  listElem.innerHTML = "";
-  renderTasks(tasks);
-};
-
-buttonElem.addEventListener("click", onButtonClick);
+console.log(getLocalStorageData());
