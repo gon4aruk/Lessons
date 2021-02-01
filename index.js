6f757eff8c412c2bac43953cc93a0019bb510914
gon4aruk <sashagonch27@gmail.com>
2021-02-01 12:45:35 +0200

    lessons

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-export const getLocalStorageData = () => {
-  return Object.entries(localStorage).reduce((acc, [key, value]) => {
-    let newValue;
-    try {
-      newValue = JSON.parse(value);
-    } catch (error) {
-      newValue = value;
-    }
-    return {
-      ...acc,
-      [key]: newValue,
-    };
-  }, {});
+const counterElem = document.querySelector(".counter");
+const counterValueElem = document.querySelector(".counter__value");
+
+const onCounterChange = (event) => {
+  const isButton = event.target.classList.contains("counter__button");
+
+  if (!isButton) {
+    return;
+  }
+
+  const oldValue = Number(counterValueElem.textContent);
+
+  const { action: dataAction } = event.target.dataset;
+
+  counterValueElem.textContent =
+    dataAction === "increase" ? oldValue + 1 : oldValue - 1;
+
+  const newValue = counterValueElem.textContent;
+
+  localStorage.setItem("counterValue", newValue);
 };
+
+counterElem.addEventListener("click", onCounterChange);
+
+const onStorageChange = (event) => {
+  counterValueElem.textContent = event.newValue;
+};
+
+window.addEventListener("storage", onStorageChange);
+
+const onDocumentLoaded = () => {
+  counterValueElem.textContent = localStorage.getItem("counterValue");
+};
+
+document.addEventListener("DOMContentLoaded", onDocumentLoaded);
