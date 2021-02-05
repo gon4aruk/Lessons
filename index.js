be57ebbb360c5d1c56ae3ffb7b61da7429ab4e46
gon4aruk <sashagonch27@gmail.com>
2021-02-05 14:37:36 +0200

    lesson

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-export const addImageV2 = url => {
-  const promise = new Promise((resolve, reject) => {
-    const img = document.createElement('img');
-    img.setAttribute('alt', 'User avatar');
-    img.src = url;
-
-    const pageElem = document.querySelector('.page');
-    pageElem.append(img);
-
-    const onImageLoaded = () => {
-      const { width, height } = img;
-      resolve({ width, height });
-    };
-
-    const onImageLoadError = () => reject(new Error('Image load failed'));
-
-    img.addEventListener('load', onImageLoaded);
-
-    img.addEventListener('error', onImageLoadError);
-  });
-
-  return promise;
-};
+export const asyncCalculator = (num) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`Initial value: ${num}`);
+      resolve(num);
+    }, 500);
+  })
+    .then(
+      (value) =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            const result = value * value;
+            console.log(`Squared value: ${result}`);
+            resolve(result);
+          }, 500);
+        })
+    )
+    .then(
+      (value) =>
+        new Promise((resolve) => {
+          const result = value * 2;
+          console.log(result);
+          resolve(result);
+        })
+    );
 
