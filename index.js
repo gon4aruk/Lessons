f0939ff09250a96abe58f02a3b673a2e286f814b
gon4aruk <sashagonch27@gmail.com>
2021-02-22 20:54:17 +0200

    lesson

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
-const userAvatarElem = document.querySelector(".user__avatar");
-const userNameElem = document.querySelector(".user__name");
-const userLocationElem = document.querySelector(".user__location");
+const baseUrl = "https://60322069081a010017547728.mockapi.io/api/v1/users";
 
-const renderUserData = (userData) => {
-  const { avatar_url, name, location } =  userData;
-  userAvatarElem.src = avatar_url;
-  userNameElem.textContent = name;
-  userLocationElem.textContent = location
-    ? `from ${location}`
-    : '';
-}
+const formElem = document.querySelector(".login-form");
+const emailInputElem = document.querySelector('.form-input[type="email"]');
+const userNameInputElem = document.querySelector('.form-input[type="text"]');
+const passwordInputElem = document.querySelector(
+  '.form-input[type="password"]'
+);
+const submitBtnElem = document.querySelector(".submit-button");
+const errorTextElem = document.querySelector(".error-text");
 
-const fetchUserData = (userName) => {
-  return fetch(`https://api.github.com/users/${userName}`)
-    .then(response => response.json());
+const checkValidation = () => {
+  if (formElem.reportValidity()) {
+    submitBtnElem.removeAttribute("disabled");
+  }
+
+  errorTextElem.textContent = "";
 };
 
-const showUserButtonElem = document.querySelector(".name-form__btn");
-const userNameInputElem = document.querySelector(".name-form__input");
+const onSubmitForm = (event) => {
+  event.preventDefault();
 
-const onClickButton = () => {
+  const email = emailInputElem.value;
   const userName = userNameInputElem.value;
-  fetchUserData(userName)
-    .then(userData => renderUserData(userData));
+  const password = passwordInputElem.value;
+
+  formElem.reset();
+
+  const userData = {
+    email,
+    userName,
+    password,
+  };
+
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((response) => response.json())
+    .then((result) => alert(JSON.stringify(result)))
+    .catch(() => {
+      errorTextElem.textContent = "Failed to create user";
+    });
 };
 
-showUserButtonElem.addEventListener("click", onClickButton);
+formElem.addEventListener("input", checkValidation);
+submitBtnElem.addEventListener("click", onSubmitForm);
