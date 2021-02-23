e0d51ccf8b4dc80d8ae955cf6d1836fe64c27d54
gon4aruk <sashagonch27@gmail.com>
2021-02-23 11:38:09 +0200

    lesson

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const baseUrl = "https://60322069081a010017547728.mockapi.io/api/v1/users";
 
 const formElem = document.querySelector(".login-form");
-const emailInputElem = document.querySelector('.form-input[type="email"]');
-const userNameInputElem = document.querySelector('.form-input[type="text"]');
-const passwordInputElem = document.querySelector(
-  '.form-input[type="password"]'
-);
 const submitBtnElem = document.querySelector(".submit-button");
 const errorTextElem = document.querySelector(".error-text");
 
@@ -20,9 +15,10 @@ const checkValidation = () => {
 const onSubmitForm = (event) => {
   event.preventDefault();
 
-  const email = emailInputElem.value;
-  const userName = userNameInputElem.value;
-  const password = passwordInputElem.value;
+  const formData = new FormData(formElem);
+  const email = formData.get("email");
+  const userName = formData.get("name");
+  const password = formData.get("password");
 
   const userData = {
     email,
@@ -30,21 +26,17 @@ const onSubmitForm = (event) => {
     password,
   };
 
-  const stringUserData = JSON.stringify(userData);
-
-  const response = fetch(baseUrl, {
+  fetch(baseUrl, {
     method: "POST",
-    body: stringUserData,
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
-  });
-
-  response
-    .then((result) => result.json())
+    body: JSON.stringify(userData),
+  })
+    .then((response) => response.json())
     .then((data) => {
       alert(JSON.stringify(data));
-      errorTextElem.textContent = "";
+      formElem.reset();
     })
     .catch(() => {
       errorTextElem.textContent = "Failed to create user";
